c4b8e191a2f344cb43fbf388c28dfa129dfdbc87
Ambjörn Fagerström <ambjorn89@gmail.com>
2023-06-16 10:54:29 +0200

    Fix: check kundnummer

--- pages/kundnummer.tsx
+++ pages/kundnummer.tsx
@@ -28,22 +28,30 @@ export default function KundNummer() {
         <h1 className="mb-8 text-center text-4xl font-black leading-tight">
           Kundnummer
         </h1>
-        <div className="flex justify-between border border-transparent border-b-blue-200 p-4">
-          <b>Catell</b>
-          <span>{catell}</span>
-        </div>
-        <div className="flex justify-between border border-transparent border-b-blue-200 p-4">
-          <b>Juzo</b>
-          <span>{juzo}</span>
-        </div>
-        <div className="flex justify-between border border-transparent border-b-blue-200 p-4">
-          <b>Medema</b>
-          <span>{medema}</span>
-        </div>
-        <div className="flex justify-between border border-transparent border-b-blue-200 p-4">
-          <b>MediqSverige</b>
-          <span>{mediqSverige}</span>
-        </div>
+        {catell && (
+          <div className="flex justify-between border border-transparent border-b-blue-200 p-4">
+            <b>Catell</b>
+            <span>{catell}</span>
+          </div>
+        )}
+        {juzo && (
+          <div className="flex justify-between border border-transparent border-b-blue-200 p-4">
+            <b>Juzo</b>
+            <span>{juzo}</span>
+          </div>
+        )}
+        {medema && (
+          <div className="flex justify-between border border-transparent border-b-blue-200 p-4">
+            <b>Medema</b>
+            <span>{medema}</span>
+          </div>
+        )}
+        {mediqSverige && (
+          <div className="flex justify-between border border-transparent border-b-blue-200 p-4">
+            <b>MediqSverige</b>
+            <span>{mediqSverige}</span>
+          </div>
+        )}
       </div>
     </>
   );
