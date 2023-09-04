85a0f384f3bf33c093acb5060e419e53bcbfef5a
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2023-09-04 20:32:45 +0200

    Add ga scripy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,25 +6,39 @@ import { AuthProvider } from "../hooks/useAuth";
 import Layout from "../components/layout";
 import "../styles/index.css";
 import { SessionProvider } from "next-auth/react";
+import Script from "next/script";
 
 const lato = Lato({
-  weight: ["400", "700", "900"],
-  style: ["normal", "italic"],
-  subsets: ["latin"],
-  variable: "--font-lato",
+weight: ["400", "700", "900"],
+style: ["normal", "italic"],
+subsets: ["latin"],
+variable: "--font-lato",
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <SessionProvider session={pageProps.session}>
-      <ApolloProvider client={client}>
-        <main className={`pt-20 ${lato.variable} font-sans`}>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </main>
-      </ApolloProvider>
-    </SessionProvider>
+  <SessionProvider session={pageProps.session}>
+    <ApolloProvider client={client}>
+      <main className={`pt-20 ${lato.variable} font-sans`}>
+        <Layout>
+          {/* Google tag (gtag.js) */}
+            <Script
+              strategy="afterInteractive"
+              src="https://www.googletagmanager.com/gtag/js?id=G-DCHPCF2P9B"
+            />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments)}
+            gtag('js', new Date());
+            gtag('config', 'G-DCHPCF2P9B');
+            `}
+          </Script>
+        <Component {...pageProps} />
+        </Layout>
+      </main>
+    </ApolloProvider>
+  </SessionProvider>
   );
 }
 
